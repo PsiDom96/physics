7e9c520cfa0bda2f41ff11b4f1af5664e0c17f73
DominicDitmyer <ditmyer.dominic@gmail.com>
2026-06-21 13:30:56 -0700

    Reorganize Vibr into installable Python package

--- vibr/vibr/materials/database.py
+++ vibr/vibr/materials/database.py
@@ -1,5 +1,6 @@
 MATERIALS = {
     "steel": {"E": 200e9, "rho": 7800, "nu": 0.3},
     "aluminum": {"E": 70e9, "rho": 2700, "nu": 0.33},
+    "bob":{"love": 700, "other_prop": 1.2453e67}
     # etc.
 }
